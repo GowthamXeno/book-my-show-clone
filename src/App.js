6c4507e0647ee2c5990acbe81d238a4856118670
GowthamXeno <gowthamudpm2004@gmail.com>
2024-02-28 06:45:00 +0530

    Updated

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import './App.css';
 import {Routes , Route} from "react-router-dom"
 
-
-
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 function App() {
   return (

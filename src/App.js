40ef868523190bea41d99cc9b80365f930688b8e
GowthamXeno <gowthamudpm2004@gmail.com>
2024-02-28 22:10:51 +0530

    Updated

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
 import './App.css';
-import {Routes , Route} from "react-router-dom"
+import {Routes , Route} from "react-router-dom";
+
+import HomePage from './Pages/HomePage';
+import MoviePage from './Pages/MoviePage';
+import PlayPage from './Pages/PlayPage';
 
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
@@ -7,9 +11,9 @@ import "slick-carousel/slick/slick-theme.css";
 function App() {
   return (
    <Routes>
-      <Route path="/" ></Route>
-      <Route path="/movie/:id" ></Route>
-      <Route path="/plays" ></Route>
+      <Route path="/" element={<HomePage />}></Route>
+      <Route path="/movie/:id" element={<MoviePage />}></Route>
+      <Route path="/plays" element={<PlayPage />}></Route>
    </Routes>
   );
 }
